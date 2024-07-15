96a0488fe308bef0de9dea59da5b62b8e958f5f8
qiuyu.yang <qiuyu.yang@realai.ai>
2024-07-15 18:08:48 +0800

    checkpoint

--- hummingbot/connector/derivative/my_jojo_perpetual/my_jojo_perpetual_constants.py
+++ hummingbot/connector/derivative/my_jojo_perpetual/my_jojo_perpetual_constants.py
@@ -1,8 +1,10 @@
+from hummingbot.core.api_throttler.data_types import RateLimit
+
 EXCHANGE_NAME = "my_jojo_perpetual"
 BROKER_ID = "HBOT"
 MAX_ORDER_ID_LEN = None
 
-DOMAIN = "my_jojo_perpetual_main"
+DOMAIN = "my_jojo_perpetual"
 TESTNET_DOMAIN = "my_jojo_perpetual_testnet"
 
 API_VERSION = "v1"
@@ -16,3 +18,17 @@ TESTNET_BASE_URL = "https://api.base-sepolia.jojo.exchange"
 PERPETUAL_WS_URL = "wss://stream.base-mainnet.jojo.exchange"
 
 TESTNET_WS_URL = "wss://stream.base-sepolia.jojo.exchange"
+
+RATE_LIMITS = [
+    RateLimit(limit_id="COMMON", limit=2400, time_interval=60),
+    RateLimit(limit_id="ORDER", limit=1200, time_interval=60),
+]
+
+if __name__ == "__main__":
+    from pprint import pprint
+    import requests
+    from urllib.parse import urljoin
+
+    url = urljoin(PERPETUAL_BASE_URL, "/v1/exchangeInfo")
+    response = requests.get(url)
+    pprint(response.json())
---
+++ hummingbot/connector/derivative/my_jojo_perpetual/my_jojo_perpetual_utils.py
@@ -0,0 +1,82 @@
+from decimal import Decimal
+
+from pydantic import Field, SecretStr
+
+from hummingbot.client.config.config_data_types import BaseConnectorConfigMap, ClientFieldData
+from hummingbot.core.data_type.trade_fee import TradeFeeSchema
+
+# https://api.base-mainnet.jojo.exchange/v1/exchangeInfo
+DEFAULT_FEES = TradeFeeSchema(
+    maker_percent_fee_decimal=Decimal("0"),
+    taker_percent_fee_decimal=Decimal("0.0003"),
+)
+
+CENTRALIZED = False
+
+EXAMPLE_PAIR = "btcusdc"
+
+
+class MyJojoPerpetualConfigMap(BaseConnectorConfigMap):
+    connector: str = Field(default="my_jojo_perpetual", client_data=None)
+    my_jojo_perpetual_api_secret: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Base wallet private key",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        ),
+    )
+    my_jojo_perpetual_api_key: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Base address",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        ),
+    )
+
+    class Config:
+        title = "my_jojo_perpetual"
+
+
+KEYS = MyJojoPerpetualConfigMap.construct()
+
+OTHER_DOMAINS = ["my_jojo_perpetual_testnet"]
+OTHER_DOMAINS_PARAMETER = {"my_jojo_perpetual_testnet": "my_jojo_perpetual_testnet"}
+OTHER_DOMAINS_EXAMPLE_PAIR = {"my_jojo_perpetual_testnet": "btcusdc"}
+OTHER_DOMAINS_DEFAULT_FEES = {
+    "my_jojo_perpetual_testnet": TradeFeeSchema(
+        maker_percent_fee_decimal=Decimal("-0.0001"),
+        taker_percent_fee_decimal=Decimal("0.0003"),
+    )
+}
+
+
+class MyJojoPerpetualTestnetConfigMap(BaseConnectorConfigMap):
+    connector: str = Field(default="my_jojo_perpetual_testnet", client_data=None)
+    my_jojo_perpetual_api_secret: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Base wallet private key",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        ),
+    )
+    my_jojo_perpetual_api_key: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Base address",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        ),
+    )
+
+    class Config:
+        title = "my_jojo_perpetual_testnet"
+
+
+OTHER_DOMAINS_KEYS = {"my_jojo_perpetual_testnet": MyJojoPerpetualTestnetConfigMap.construct()}
