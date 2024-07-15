d0324331da014f1ad1a1d852c0a0c5c9ea4de571
QIUYU YANG <wwwyqy618@gmail.com>
2024-07-15 09:14:10 +0800

    checkpoint

---
+++ hummingbot/connector/derivative/my_jojo_perpetual/my_jojo_perpetual_constants.py
@@ -0,0 +1,18 @@
+EXCHANGE_NAME = "my_jojo_perpetual"
+BROKER_ID = "HBOT"
+MAX_ORDER_ID_LEN = None
+
+DOMAIN = "my_jojo_perpetual_main"
+TESTNET_DOMAIN = "my_jojo_perpetual_testnet"
+
+API_VERSION = "v1"
+
+SERVER_TIME_PATH_URL = f"{API_VERSION}/time"
+
+PERPETUAL_BASE_URL = "https://api.base-mainnet.jojo.exchange"
+
+TESTNET_BASE_URL = "https://api.base-sepolia.jojo.exchange"
+
+PERPETUAL_WS_URL = "wss://stream.base-mainnet.jojo.exchange"
+
+TESTNET_WS_URL = "wss://stream.base-sepolia.jojo.exchange"
